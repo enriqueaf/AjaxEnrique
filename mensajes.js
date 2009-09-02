0ae66cecd947262ca9a5749448d40be74fbd6055
Enrique Alvarez <enriqueaf@gmail.com>
2009-09-02 16:42:25 +0200

    Desactivando logs para firebug.

--- mensajes.js
+++ mensajes.js
@@ -1,11 +1,13 @@
 //Clase para la presentación de los mensajes. Ejm: Firebug, Desktop ...
 Ev.mensaje = {};
 Ev.mensaje.debug = function(a){
-	if(Ev.var.debug){console.log(a)};
+	if(Ev.var.debug){
+		//console.log(a)
+		};
 };
 Ev.mensaje.log = function(a){
-	console.log(a);
+	//console.log(a);
 };
 Ev.mensaje.error = function(a){
-	console.log(a);
+	//console.log(a);
 };
